5b29659aff414e68a0987c86421a9eeb01c82bba
paluku-ezechiel <palukuezechiel@gmail.com>
2026-08-13 15:17:51 +0200

    feat: utilisation de fetch pour recuperer les donnees

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import "./assets/styles/styles.scss";
 import "./index.scss";
 
+const fetchArticle = async () => {
+  try {
+    const response = await fetch("https://restapi.fr/api/article");
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP (${response.status})`);
+    }
+    const article = await response.json();
+    createArticles(article);
+  } catch (error) {}
+};
 
-const fetchArticle = () => {
-    
-}
+fetchArticle();
